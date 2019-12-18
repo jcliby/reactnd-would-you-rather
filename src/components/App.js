0f3beee4f2e22b1af3ae7df1ebb0ac546ce61886
Jonathan Cliby <jon.cliby@gmail.com>
2019-12-18 07:52:03 -0600

    Load initial data

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { handleInitialData } from '../actions/shared';
 
-function App() {
-  return <div>Temp</div>;
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+  render() {
+    return <div>Temp</div>;
+  }
 }
 
-export default App;
+export default connect()(App);
