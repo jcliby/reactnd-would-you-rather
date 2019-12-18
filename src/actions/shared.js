105fb9a0fecad360a6af3f9eaa6a9af89ea50c4d
Jonathan Cliby <jon.cliby@gmail.com>
2019-12-18 12:52:59 -0600

    Add loading bar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,15 +2,18 @@ import { getInitialData } from '../utils/api';
 import { receiveQuestions } from './questions';
 import { receiveUsers } from './users';
 import { setAuthedUser } from './authedUser';
+import { showLoading, hideLoading } from 'react-redux-loading';
 
 const AUTHED_ID = 'tylermcginnis';
 
 export function handleInitialData() {
   return dispatch => {
+    dispatch(showLoading());
     return getInitialData().then(({ users, questions }) => {
       dispatch(receiveUsers(users));
       dispatch(receiveQuestions(questions));
       dispatch(setAuthedUser(AUTHED_ID));
+      dispatch(hideLoading());
     });
   };
 }
--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,22 @@ import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
 import Dashboard from './Dashboard';
 import Leaderboard from './Leaderboard';
+import LoadingBar from 'react-redux-loading';
 
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
   render() {
-    return <div>{this.props.loading === true ? null : <Leaderboard />}</div>;
+    return (
+      <div>
+        <LoadingBar
+          style={{ backgroundColor: '#b0b0b0', height: '7px' }}
+          updateTime={500}
+        />
+        {this.props.loading === true ? null : <Leaderboard />}
+      </div>
+    );
   }
 }
 
