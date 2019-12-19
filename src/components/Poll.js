467af976393911f957fe734e1f58bed2f3888390
Jonathan Cliby <jon.cliby@gmail.com>
2019-12-18 18:27:41 -0600

    Poll show question or result functionality

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { handleAddAnswer } from '../actions/questions';
-import { Button, Card, Image, Radio } from 'semantic-ui-react';
+import { Button, Card, Image, Radio, Message } from 'semantic-ui-react';
 
 class Poll extends Component {
   state = {
@@ -28,42 +28,78 @@ class Poll extends Component {
   };
 
   render() {
-    const { authedUser, question, author } = this.props;
+    const { authedUser, question, author, answer } = this.props;
+
+    const optionOneVotes = question.optionOne.votes.length;
+    const optionTwoVotes = question.optionTwo.votes.length;
+    const totalVotes = optionOneVotes + optionTwoVotes;
+
     return (
       <div>
-        <Card>
-          <Card.Content>
-            <Image
-              floated="right"
-              size="mini"
-              circular
-              src={author.avatarURL}
-            />
-            <Card.Header>Would you rather...</Card.Header>
-            <Card.Meta>
-              Created by {author.id === authedUser ? 'You' : author.name}
-            </Card.Meta>
-            <Card.Description>
-              <Radio
-                label={question.optionOne.text}
-                value={'optionOne'}
-                checked={this.state.value === 'optionOne'}
-                onChange={this.handleChange}
-              />
-              <Radio
-                label={question.optionTwo.text}
-                value="optionTwo"
-                checked={this.state.value === 'optionTwo'}
-                onChange={this.handleChange}
-              />
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <Button fluid basic onClick={this.handleSubmit}>
-              Submit
-            </Button>
-          </Card.Content>
-        </Card>
+        {answer === null ? (
+          <div>
+            <Card>
+              <Card.Content>
+                <Image
+                  floated="right"
+                  size="mini"
+                  circular
+                  src={author.avatarURL}
+                />
+                <Card.Header>Would you rather...</Card.Header>
+                <Card.Meta>
+                  Created by {author.id === authedUser ? 'You' : author.name}
+                </Card.Meta>
+                <Card.Description>
+                  <Radio
+                    label={question.optionOne.text}
+                    value={'optionOne'}
+                    checked={this.state.value === 'optionOne'}
+                    onChange={this.handleChange}
+                  />
+                  <Radio
+                    label={question.optionTwo.text}
+                    value="optionTwo"
+                    checked={this.state.value === 'optionTwo'}
+                    onChange={this.handleChange}
+                  />
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <Button fluid basic onClick={this.handleSubmit}>
+                  Submit
+                </Button>
+              </Card.Content>
+            </Card>
+          </div>
+        ) : (
+          <div>
+            <Card>
+              <Card.Content>
+                <Image
+                  floated="right"
+                  size="mini"
+                  circular
+                  src={author.avatarURL}
+                />
+                <Card.Header>Would you rather...</Card.Header>
+                <Card.Meta>
+                  Created by {author.id === authedUser ? 'You' : author.name}
+                </Card.Meta>
+                <Card.Description>
+                  <Message color={answer === 'optionOne' ? 'green' : 'grey'}>
+                    <Message.Header>{question.optionOne.text}</Message.Header>
+                    <p>{`${optionOneVotes} out of ${totalVotes} votes`}</p>
+                  </Message>
+                  <Message color={answer === 'optionTwo' ? 'green' : 'grey'}>
+                    <Message.Header>{question.optionTwo.text}</Message.Header>
+                    <p>{`${optionTwoVotes} out of ${totalVotes} votes`}</p>
+                  </Message>
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          </div>
+        )}
       </div>
     );
   }
@@ -72,10 +108,21 @@ class Poll extends Component {
 function mapStateToProps({ authedUser, questions, users }, { id }) {
   const question = questions[id];
   const author = users[question.author];
+  let answer = null;
+
+  if (question.optionOne.votes.includes(authedUser)) {
+    answer = 'optionOne';
+  }
+
+  if (question.optionTwo.votes.includes(authedUser)) {
+    answer = 'optionTwo';
+  }
+
   return {
     authedUser,
     question,
-    author
+    author,
+    answer
   };
 }
 
